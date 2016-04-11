ceef10fbbe96580f7dc7dfff9177b63b299a5f36
Xaria Kirtikar <xak200@nyu.edu>
2016-04-10 21:59:50 -0400

    extra

--- Database Design & Implementation/Pre-Lamp/Kirtikar_Xaria_Assignment5/dataEntry.sql
+++
@@ -1,5 +0,0 @@
-mysql> CREATE TABLE users (user_email VARCHAR(30) NOT NULL, phone VARCHAR(15) NOT NULL, first_name VARCHAR(20) NOT NULL, last_name VARCHAR(20));
-Query OK, 0 rows affected (0.03 sec)
-
-mysql> CREATE TABLE user_interest(user_interest_id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, user_email VARCHAR(30) NOT NULL, listing_id INT(10), PRIMARY KEY(user_id));
-Query OK, 0 rows affected (0.01 sec)
